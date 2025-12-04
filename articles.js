9f6133b7dbc7e089a6fb6fccc600038d99de8903
Girard Thomas <104352497+Tomalexis@users.noreply.github.com>
2025-12-04 18:15:43 +0100

    Fix image paths in articles.js
    Updated image paths to remove leading slashes.

--- articles.js
+++ articles.js
@@ -1,36 +1,36 @@
 export const articles = [
     {
-        image: '/images/building.jpg',
+        image: 'images/building.jpg',
         date: 'December 3rd 2025',
         title: 'Looking up',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
     },
     {
-        image: '/images/beach.jpg',
+        image: 'images/beach.jpg',
         date: 'December 1st 2025',
         title: 'Beach time',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
     },
     {
-        image: '/images/school.jpg',
+        image: 'images/school.jpg',
         date: 'December 1st 2025',
         title: 'Article 3',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
     },
     {
-        image: '/images/plane.jpg',
+        image: 'images/plane.jpg',
         date: 'December 1st 2025',
         title: 'Article 4',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
     },
     {
-        image: '/images/subway.jpg',
+        image: 'images/subway.jpg',
         date: 'December 1st 2025',
         title: 'Article 5',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
     },
     {
-        image: '/images/cinema.jpg',
+        image: 'images/cinema.jpg',
         date: 'December 1st 2025',
         title: 'Article 6',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pellentesque nulla euismod enim imperdiet, ut feugiat felis faucibus.'
